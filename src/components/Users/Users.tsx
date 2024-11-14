745165eb9fac4b692d5974461d389f26e9078517
Eugene <admin@MacBook-Pro-Evgenij.local>
2024-11-14 19:32:17 +0200

    51

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,32 +1,49 @@
 import React from 'react';
-import axios from "axios";
-
+import axios from 'axios';
+import userPhoto from '../../assets/images/user.png';
 
 const Users = (props: any) => {
-    if (props.users.length === 0){
-        axios.get('http://social-network.samuraijs.com/api/1.0/users').then(res => console.log(res))
-    }
+  if (props.users.length === 0) {
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then((res) => {
+      props.setUsers(res.data.items);
+    });
+  }
 
-    return (
-        <div>
-            {
-                props.users.map((u: any) => <div key={u.id}>
-                    <div>
-                        <img src={u.urlPhoto} alt=""/>
-                        {u.isFollow ? <button onClick={() => {props.unFollow(u.id)}}>unfollow</button> : <button onClick={() => {props.follow(u.id)}}>follow</button> }
-                    </div>
-                    <div>
-                        <div>{u.fullName}</div>
-                        <div>{u.status}</div>
-                    </div>
-                    <div>
-                        <div>{u.location.city}</div>
-                        <div>{u.location.country}</div>
-                    </div>
-                </div>)
-            }
+  return (
+    <div>
+      {props.users.map((u: any) => (
+        <div key={u.id}>
+          <div>
+            <img
+              style={{ width: '150px' }}
+              src={u.photos.small !== null ? u.photos.small : userPhoto}
+              alt=""
+            />
+            {u.isFollow ? (
+              <button
+                onClick={() => {
+                  props.unFollow(u.id);
+                }}>
+                unfollow
+              </button>
+            ) : (
+              <button
+                onClick={() => {
+                  props.follow(u.id);
+                }}>
+                follow
+              </button>
+            )}
+          </div>
+          <div>
+            <div>{u.name}</div>
+            <div>{u.status}</div>
+          </div>
+          <div></div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Users;
